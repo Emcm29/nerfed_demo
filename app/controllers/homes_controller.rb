fd1e52985919ca856c52325367ad174f154bd705
Eoin <eoin@ubuntu.(none)>
2013-04-18 15:18:18 -0700

    commented out translation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 	def index
-		translator = BingTranslator.new('EoinTestTranslation1', 'hNqLMwhxlXVm39j+zi61h45+zWwBooT4W+v5CacLtjs=')
-		@spanish = translator.translate 'Hello. This will be translated!', :from => 'en', :to => 'es'
+		#translator = BingTranslator.new('EoinTestTranslation1', 'hNqLMwhxlXVm39j+zi61h45+zWwBooT4W+v5CacLtjs=')
+		#@spanish = translator.translate 'Hello. This will be translated!', :from => 'en', :to => 'es'
+		@translation = 'insert translation here'
 	end
 end
