16905c4fa49e3d5fc3f9aa29ed78230cf390e7f5
Eoin <eoin@ubuntu.(none)>
2013-04-20 11:54:54 -0700

    changed production.rb to precompile all .js files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Demo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.assets.precompile << '*.js'
   # Code is not reloaded between requests
   config.cache_classes = true
 
