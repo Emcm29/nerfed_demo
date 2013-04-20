e1ab4573d0be7e1685cb028d7774fc5f564eac62
Eoin <eoin@ubuntu.(none)>
2013-04-20 14:43:29 -0700

    Hopfully fixed translation

--- app/controllers/ctranslates_controller.rb
+++ app/controllers/ctranslates_controller.rb
@@ -19,7 +19,7 @@ class CtranslatesController < ApplicationController
 		@text = params[:text]
 		#translation magic
 		translator = BingTranslator.new('EoinTestTranslation1', 'hNqLMwhxlXVm39j+zi61h45+zWwBooT4W+v5CacLtjs=')
-		@translation = translator.translate @source, :from => @@from, :to => @@to
+		@translation = translator.translate @text, :from => @@from, :to => @@to
 		#@translation = 'silly'
 		@base = Ctranslation.first(:order => "id DESC")
 
