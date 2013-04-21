f6f926fcb1586efbdaab482f323cae66c8a7ea19
Eoin <eoin@ubuntu.(none)>
2013-04-21 14:23:11 -0700

    fixed ajax in home#show

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,7 +1,7 @@
 //show.js
 
 $(document).ready(function(){
-	setinterval(update_shown, 5000);
+	var tid = setInterval(update_shown, 5000);
 
 	function update_shown(){
 		$('#shown').load("homes/new");
