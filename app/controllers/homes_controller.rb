50bb00dd3d81eb9e67f8b7de12b43f445df4e765
Eoin <eoin@ubuntu.(none)>
2013-04-21 13:10:25 -0700

    Added back in translation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,12 @@ class HomesController < ApplicationController
 		#@spanish = translator.translate 'Hello. This will be translated!', :from => 'en', :to => 'es'
 		@translation = 'insert translation here'
 	end
+
+	def show
+		@base = Ctranslation.first(:order => "id DESC") 
+	end
+
+	def new
+
+	end
 end
