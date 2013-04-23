4ddb6471c310c114878b825bde6658b2ffbec36d
Eoin <eoin@ubuntu.(none)>
2013-04-23 11:24:43 -0700

    Added translation box

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
 		#translator = BingTranslator.new('EoinTestTranslation1', 'hNqLMwhxlXVm39j+zi61h45+zWwBooT4W+v5CacLtjs=')
 		#@spanish = translator.translate 'Hello. This will be translated!', :from => 'en', :to => 'es'
 		@translation = 'insert translation here'
+		@base = Ctranslation.first(:order => "id DESC")
 	end
 
 	def show
