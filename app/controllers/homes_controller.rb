18e80df6c8d9c315d043290dea3b1aab9984683d
emcm29 <emcm29@gmail.com>
2013-04-23 12:12:29 -0700

    Removed some verbose

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,5 @@
 class HomesController < ApplicationController
 	def index
-		#translator = BingTranslator.new('EoinTestTranslation1', 'hNqLMwhxlXVm39j+zi61h45+zWwBooT4W+v5CacLtjs=')
-		#@spanish = translator.translate 'Hello. This will be translated!', :from => 'en', :to => 'es'
-		@translation = 'insert translation here'
 		@base = Ctranslation.first(:order => "id DESC")
 	end
 
